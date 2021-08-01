414d959259e4de429720601ac0c8ec45b42cd160
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-07-31 21:33:54 -0500

    Add game header, and some other refactoring.
    - Add header rects.
    - Change size of player to be more appropriate.
    - Code documentation

--- src/scenes/gameScene.ts
+++ src/scenes/gameScene.ts
@@ -3,6 +3,7 @@ import canBomb from '../abilities/canBomb'
 import canWalk from '../abilities/canWalk'
 import brick from '../model/Brick'
 import timer from '../model/Timer'
+import {convertMapPosToCoord, findMapItem} from '../util'
 import {GRID_PIXEL_SIZE} from '../types'
 
 const DEFAULT_GAME_TIME = 180
@@ -12,6 +13,7 @@ const {
     addLevel, 
     area,
     collides,
+    color,
     debug,
     destroy,
     go, 
@@ -20,6 +22,7 @@ const {
     layer,
     layers, 
     pos, 
+    rect,
     scale, 
     solid,
     sprite,
@@ -30,7 +33,7 @@ const {
 
 const map = [
     '###############',
-    '#   OO OOOO   #',
+    '#1  OO OOOO  2#',
     '# #O###O#O### #',
     '# OOO OO#OOOO #',
     '#O###O# ###O#O#',
@@ -40,39 +43,63 @@ const map = [
     '#O#O### #O###O#',
     '# OOO #OOO OO #',
     '# ###O#O###O# #',
-    '#   OOOO OO   #',
+    '#4  OOOO OO  3#',
     '###############',
 ]
 
+const noArea = true
+
+const mapWidth = map[0].length
+const mapWidthPixels = mapWidth * GRID_PIXEL_SIZE
+const mapHeight = map.length
+const mapHeightPixels = mapHeight * GRID_PIXEL_SIZE
+
+const WHITE = color(1, 1, 1)
+const BLUE = color(0.39, 0.47, 0.937)
+const GREEN = color(0.223, 0.517, 0)
+
 export default function () {
+    
+    // Layers
     layers(['bg', 'bomb', 'obj', 'ui'], 'obj')
+    
+    // Level/Map
     const mapConfig = {
         width: GRID_PIXEL_SIZE,
         height: GRID_PIXEL_SIZE,
+        pos: vec2(0, GRID_PIXEL_SIZE),
         scale: 2,
         '#': [sprite('block'), scale(2), solid(), 'block'],
         'O': [sprite('brick'), scale(2), solid(), brick(), 'brick'],
         any: (ch) => null,
     }
     addLevel(map, mapConfig)
+    add([
+        rect(mapWidthPixels, mapHeightPixels, {noArea}),
+        pos(0, GRID_PIXEL_SIZE),
+        GREEN,
+        layer('bg'),
+    ])
+
+    // Header/Timer
+    add([rect(mapWidthPixels-1, GRID_PIXEL_SIZE-1, {noArea}), WHITE])
+    add([rect(mapWidthPixels-5, GRID_PIXEL_SIZE-5, {noArea}), pos(2, 2), BLUE])
     const timerLabel = add([
-        text("", 18),
-        pos(15, 6),
+        text("", 16),
+        pos(12, 9),
         timer(DEFAULT_GAME_TIME),
     ]);
     timerLabel.on('timer_end', ()=>{
         go('lose')
     })
-    add([
-        sprite('space'),
-        scale(15*2, 13*2),
-        layer('bg'),
-    ])
+
+    // Player
+    const {x: playerX, y: playerY} = convertMapPosToCoord(findMapItem(map, '1'))
     const player = add([
         sprite('bomberman'),
-        pos(GRID_PIXEL_SIZE, GRID_PIXEL_SIZE),
-        scale(0.92),
-        area(vec2(9,5), vec2(25,32)),
+        pos(playerX-7, playerY-12),
+        scale(1.3),
+        area(vec2(8,10), vec2(26,34)),
         canBomb(),
         canWalk(),
         'player',
@@ -86,6 +113,8 @@ export default function () {
             go('lose')
         })
     })
+
+    // Controls
     keyPress('space', ()=>{
         timerLabel.start()
         player.spawnBomb()
