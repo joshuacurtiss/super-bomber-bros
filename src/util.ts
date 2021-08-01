0d1eb9d237b8f1839a30ee8d43ab2839f14275ac
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-07-31 21:25:27 -0500

    Add some map utility methods.
    - findMapItems: Finds all positions on the map where a given item exists.
    - findMapitem: Finds the FIRST position on the map where a given item exists.
    - convertMapPosToCoord: Takes a map position and converts it into screen coordinates on the screen.

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,8 @@
 import {k} from './kaboom'
 import {Vec2} from 'kaboom'
+import {GRID_PIXEL_SIZE} from './types'
 
-const {get} = k
+const {get, vec2} = k
 
 /**
  * Returns any game objects at a given position.
@@ -14,6 +15,47 @@ function getAtPos(position:Vec2, tag?:string) {
     return objects.filter(obj=>x>=obj.pos.x && x<=obj.pos.x+obj.width && y>=obj.pos.y && y<=obj.pos.y+obj.height)
 }
 
+/**
+ * Returns map positions of a given item.
+ * @param map The map to search.
+ * @param item The item to search for.
+ */
+function findMapItems(map: string[], item: string): Vec2[] {
+    const out: Vec2[] = []
+    map.forEach((row, y)=>{
+        let x = -1
+        do {
+            x = row.indexOf(item, x+1)
+            if( x>=0 ) out.push(vec2(x, y))
+        } while( x>=0 );
+    })
+    return out
+}
+
+/**
+ * Returns the first map position of a given item.
+ * @param map The map to search.
+ * @param item The item to search for.
+ */
+function findMapItem(map: string[], item: string): Vec2 | undefined {
+    const found = findMapItems(map, item)
+    return found.length ? found[0] : undefined
+}
+
+/**
+ * Converts a map position to coordinates on the screen.
+ * @param pos Map position.
+ */
+function convertMapPosToCoord(pos:Vec2): Vec2 {
+    const {x, y} = pos
+    // Multiplies by grid pixel size, and adds row for top header
+    return vec2(x*GRID_PIXEL_SIZE, (y+1)*GRID_PIXEL_SIZE)
+}
+
+
 export {
-     getAtPos,
- }
+    findMapItems,
+    findMapItem,
+    convertMapPosToCoord,
+    getAtPos,
+}
