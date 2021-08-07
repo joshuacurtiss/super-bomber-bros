3d852bc9e88182d71325ad7741a0e8cb6d117983
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-08-06 22:35:17 -0500

    Fix speed powerup to be cumulative.
    Each speed powerup adds 30 sec. Now it will properly add 30 sec to the amount of time before you speed slows down.

--- src/abilities/canWalk.ts
+++ src/abilities/canWalk.ts
@@ -1,16 +1,18 @@
 import { Vec2 } from 'kaboom'
+import { updateLanguageServiceSourceFile } from 'typescript'
 import k from '../kaboom'
 import { POWERUPS, WALK_SPEED } from '../types'
 
 const {
     debug,
+    time,
     vec2,
-    wait,
 } = k
 
 function canWalk(speed: number = WALK_SPEED) {
     let walking=false
     let multiplier=1
+    let multiplierTimeout=0
     let dir=vec2(0,1)
     return {
         isIdle: ()=>!walking,
@@ -27,8 +29,16 @@ function canWalk(speed: number = WALK_SPEED) {
         walkPowerup(index: number) {
             if( index===POWERUPS.SPEED ) {
                 multiplier=2
-                wait(30, ()=>multiplier=1)
+                multiplierTimeout = (multiplierTimeout>0 ? multiplierTimeout : time()) + 30
                 debug.log(`Greased LIGHTNING!`)
+            }
+        },  
+        update() {
+            if( multiplierTimeout===0 ) return
+            if( time() > multiplierTimeout ) {
+                multiplierTimeout=0
+                multiplier=1
+                debug.log("Back to normal speed.")
             }
         }
     }
