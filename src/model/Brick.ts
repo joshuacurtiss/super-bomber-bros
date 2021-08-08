0f30160ef9f15f7df3fb167b2ee920d6c5e3bc48
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-08-07 22:42:53 -0500

    Change the randomization of powerups slightly.

--- src/model/Brick.ts
+++ src/model/Brick.ts
@@ -17,7 +17,7 @@ const {
 
 function randomPowerup():number {
     const validIndices = Object.values(POWERUPS)
-    const index = Math.floor(rand(0, 30))
+    const index = Math.floor(rand(-10, 25))
     return validIndices.includes(index) ? index : -1
 }
 
