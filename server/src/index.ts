0164a6c58bd3ce9319c93ae5057e74b87a38867c
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-08-16 23:44:32 -0500

    Add basic WebSocket support to server code.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,23 @@
+import * as http from 'http';
+import * as WebSocket from 'ws';
+import { AddressInfo } from 'net';
 import express from 'express';
 import path from 'path';
 
 const port = Number(process.env.PORT || 8000) + Number(process.env.NODE_APP_INSTANCE || 0);
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.use(express.json());
-
 app.use('/', express.static(path.join(__dirname, '..', '..', 'dist')));
 
-app.listen(port)
-console.log(`Listening on port ${port}.`);
+wss.on('connection', (ws: WebSocket) => {
+    ws.on('message', function message(msg) {
+        console.log(`Received message ${msg}`);
+    });
+})
+
+server.listen(port), ()=>{
+    console.log(`Listening on port ${(server.address() as AddressInfo).port}`);
+};
