cf4c81615eda5396d87be172439a99e1fcc4ebde
Josh Curtiss <J.Curtiss@rockvalleycollege.edu>
2021-09-06 22:10:11 -0500

    Refactor canControl to use classes, and provide event hooks.
    Set up interfaces and classes for `canControl`.
    
    Set up an `on` handler for the different actions.

--- client/src/abilities/canControl.ts
+++ client/src/abilities/canControl.ts
@@ -1,7 +1,16 @@
 import { k } from '../kaboom'
 import { DIRS } from '../types'
 
-export type GamepadControlDefs = {
+enum ControlActions {
+    left = 'left',
+    right = 'right',
+    up = 'up',
+    down = 'down',
+    primary = 'primary',
+    secondary = 'secondary',
+}
+
+type IGamepadControlDefs = {
     id: string;
     left: number;
     right: number;
@@ -10,8 +19,20 @@ export type GamepadControlDefs = {
     primary: number;
     secondary: number;
 }
+export class GamepadControlDefs {
+    id: string;
+    left: number;
+    right: number;
+    up: number;
+    down: number;
+    primary: number;
+    secondary: number;
+    constructor(defs: IGamepadControlDefs) {
+        Object.assign(this, defs)
+    }
+}
 
-export type KeyboardControlDefs = {
+type IKeyboardControlDefs = {
     left: string;
     right: string;
     up: string;
@@ -20,35 +41,69 @@ export type KeyboardControlDefs = {
     secondary: string;
 }
 
-export const keyboardDefaults: KeyboardControlDefs[] = [{
-    left: 'left',
-    right: 'right',
-    up: 'up',
-    down: 'down',
-    primary: 'shift',
-    secondary: '1',
-}, {
-    left: 'a',
-    right: 'd',
-    up: 'w',
-    down: 's',
-    primary: 'space',
-    secondary: '2',
-}, {
-    left: 'f',
-    right: 'h',
-    up: 't',
-    down: 'g',
-    primary: 'b',
-    secondary: '3',
-}, {
-    left: 'j',
-    right: 'l',
-    up: 'i',
-    down: 'k',
-    primary: ',',
-    secondary: '4',
-}]
+export class KeyboardControlDefs {
+    left: string;
+    right: string;
+    up: string;
+    down: string;
+    primary: string;
+    secondary: string;
+    constructor(defs:IKeyboardControlDefs) {
+        Object.assign(this, defs)
+    }
+    on(action: ControlActions, callback: ()=>void, releaseCallback?: ()=>void) {
+        const handler = action===ControlActions.primary || action===ControlActions.secondary ? keyPress : keyDown
+        handler(this[action], callback)
+        if( releaseCallback ) keyRelease(this[action], releaseCallback)
+    }
+}
+
+export const keyboardDefaults: KeyboardControlDefs[] = [
+    new KeyboardControlDefs({
+        left: 'left',
+        right: 'right',
+        up: 'up',
+        down: 'down',
+        primary: 'shift',
+        secondary: '1',
+    }), 
+    new KeyboardControlDefs({
+        left: 'a',
+        right: 'd',
+        up: 'w',
+        down: 's',
+        primary: 'space',
+        secondary: '2',
+    }), 
+    new KeyboardControlDefs({
+        left: 'f',
+        right: 'h',
+        up: 't',
+        down: 'g',
+        primary: 'b',
+        secondary: '3',
+    }), 
+    new KeyboardControlDefs({
+        left: 'j',
+        right: 'l',
+        up: 'i',
+        down: 'k',
+        primary: ',',
+        secondary: '4',
+    })
+]
+
+export const gamepadDefaults: GamepadControlDefs[] = [
+    new GamepadControlDefs({
+        id: 'Logitech Dual Action (STANDARD GAMEPAD Vendor: 046d Product: c216)',
+        left: 1,
+        right: 2,
+        up: 3,
+        down: 4,
+        primary: 5,
+        secondary: 6,
+    })
+]
 
 const {
     keyDown,
@@ -57,19 +112,23 @@ const {
 } = k
 
 export default function(
-    defs: KeyboardControlDefs,
+    defs: GamepadControlDefs | KeyboardControlDefs,
     gamepad?: Gamepad
 ) {
     return {
         add() {
-            keyPress(defs.primary, ()=>this.spawnBomb())
-            keyPress(defs.secondary, ()=>console.log(this))
-            Object.entries(DIRS).forEach(([key, vec])=>{
-                const dir = key.toLowerCase()
-                if( key==='IDLE' ) return
-                keyDown(defs[dir], ()=>this.walk(vec))
-                keyRelease(defs[dir], ()=>this.stop())
-            })        
+            if( defs instanceof GamepadControlDefs ) {
+                console.log("Add gamepad")
+            } else if( defs instanceof KeyboardControlDefs ) {
+                defs.on(ControlActions.primary, ()=>this.spawnBomb())
+                defs.on(ControlActions.secondary, ()=>console.log(this))
+                defs.on(ControlActions.up, ()=>this.walk(DIRS.UP))
+                defs.on(ControlActions.down, ()=>this.walk(DIRS.DOWN))
+                defs.on(ControlActions.left, ()=>this.walk(DIRS.LEFT))
+                defs.on(ControlActions.right, ()=>this.walk(DIRS.RIGHT))
+            } else {
+                console.log("Unknown def type.")
+            }
         },
     }
 }
